37185b65771e63e5322aed988707d88660af7512
Alxalindayu <alexlouiealindayu11@gmail.com>
2017-06-28 15:12:34 +0800

    updated db

--- rg.sql
+++ rg.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
+-- version 4.0.9
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2017 at 06:07 PM
--- Server version: 5.6.20
+-- Generation Time: Jun 28, 2017 at 09:10 AM
+-- Server version: 5.6.14
 -- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `rg`
@@ -27,10 +21,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `email_sent` (
-`email_sent_id` int(11) NOT NULL,
+  `email_sent_id` int(11) NOT NULL AUTO_INCREMENT,
   `sent_to` varchar(255) NOT NULL,
   `sent_date` date NOT NULL DEFAULT '0000-00-00',
-  `status` enum('Success','Failed') NOT NULL
+  `status` enum('Success','Failed') NOT NULL,
+  PRIMARY KEY (`email_sent_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -40,12 +35,13 @@ CREATE TABLE IF NOT EXISTS `email_sent` (
 --
 
 CREATE TABLE IF NOT EXISTS `emergency_info` (
-`emergency_id` int(11) NOT NULL,
+  `emergency_id` int(11) NOT NULL AUTO_INCREMENT,
   `unique_id` varchar(255) NOT NULL,
   `contact_name` varchar(255) DEFAULT NULL,
   `contact_number` varchar(255) DEFAULT NULL,
   `contact_address` varchar(255) DEFAULT NULL,
-  `relation` varchar(255) DEFAULT NULL
+  `relation` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`emergency_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
@@ -62,10 +58,11 @@ INSERT INTO `emergency_info` (`emergency_id`, `unique_id`, `contact_name`, `cont
 --
 
 CREATE TABLE IF NOT EXISTS `images` (
-`image_id` int(10) NOT NULL,
+  `image_id` int(10) NOT NULL AUTO_INCREMENT,
   `unqiue_id` varchar(255) NOT NULL,
   `image_name` varchar(255) DEFAULT NULL,
-  `image_status` enum('Active','Inactive') DEFAULT 'Active'
+  `image_status` enum('Active','Inactive') DEFAULT 'Active',
+  PRIMARY KEY (`image_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -75,7 +72,7 @@ CREATE TABLE IF NOT EXISTS `images` (
 --
 
 CREATE TABLE IF NOT EXISTS `information` (
-`information_id` int(11) NOT NULL,
+  `information_id` int(11) NOT NULL AUTO_INCREMENT,
   `unique_id` varchar(255) NOT NULL,
   `vest_no` varchar(255) NOT NULL,
   `aspirant` varchar(255) DEFAULT NULL,
@@ -84,19 +81,53 @@ CREATE TABLE IF NOT EXISTS `information` (
   `lastname` varchar(255) NOT NULL,
   `middlename` varchar(255) NOT NULL,
   `birthday` date NOT NULL,
+  `bloodtype` varchar(255) NOT NULL,
   `st_address` varchar(255) NOT NULL,
   `city` varchar(255) NOT NULL,
+  `sector` int(11) NOT NULL,
   `contact_numberinf` varchar(255) NOT NULL,
   `date_added` date DEFAULT '0000-00-00',
-  `usertype` enum('aspirant','rg','ban') NOT NULL DEFAULT 'aspirant'
+  `usertype` enum('aspirant','rg','ban') NOT NULL DEFAULT 'aspirant',
+  PRIMARY KEY (`information_id`),
+  UNIQUE KEY `vest_no` (`vest_no`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `information`
 --
 
-INSERT INTO `information` (`information_id`, `unique_id`, `vest_no`, `aspirant`, `batch`, `firstname`, `lastname`, `middlename`, `birthday`, `st_address`, `city`, `contact_numberinf`, `date_added`, `usertype`) VALUES
-(2, '6683e1c81c6e3061083e766cea35886824f26c6ca8c38c53bdad6877f0989675be584bb48cfb4ac2944dc247f398e3d27be2b29700aabff94d1f4e68c4361ef9', '3618', '394', 23, 'Alex', 'Alindayu', '', '1995-02-11', '#9 mangga rd z-1 north signal village', 'taguig', '09396154921', '2017-06-03', 'aspirant');
+INSERT INTO `information` (`information_id`, `unique_id`, `vest_no`, `aspirant`, `batch`, `firstname`, `lastname`, `middlename`, `birthday`, `bloodtype`, `st_address`, `city`, `sector`, `contact_numberinf`, `date_added`, `usertype`) VALUES
+(2, '6683e1c81c6e3061083e766cea35886824f26c6ca8c38c53bdad6877f0989675be584bb48cfb4ac2944dc247f398e3d27be2b29700aabff94d1f4e68c4361ef9', '3618', '394', 23, 'Alex', 'Alindayu', '', '1995-02-11', 'A-positive', '#9 mangga rd z-1 north signal village', 'taguig', 3, '09396154921', '2017-06-28', 'aspirant');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sectors`
+--
+
+CREATE TABLE IF NOT EXISTS `sectors` (
+  `sector_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sector_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`sector_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `sectors`
+--
+
+INSERT INTO `sectors` (`sector_id`, `sector_name`) VALUES
+(1, 'RGEN NCR NORTH (Caloocan / Malabon / Navotas / \nValenzuela)'),
+(2, 'RGEN NCR SOUTH  (Las Pinas / Paranaque / Muntinlupa /\nPasay / Makati)'),
+(3, 'RGEN NCR EAST (Marikina / Pasig / Taguig / Pateros)'),
+(4, 'RGEN NCR WEST (Manila)'),
+(5, 'RGEN NCR CENTRAL (San Juan / Mandaluyong / Quezon City)'),
+(6, 'RGEN REGION 1 (Ilocos Norte / Ilocos Sur / La Union / \nPangasinan)'),
+(7, 'RGEN REGION 2 (Batanes / Cagayan / Isabela / \nNueva Vizcaya / Quirino)'),
+(8, 'RGEN CAR ( Abra / Apayao / Benguet / Ifugao / Kalinga\nMountain Province)'),
+(9, 'RGEN REGION 3 (Bulacan / Pampanga / Tarlac / Nueva Ecija \nBataan / Zambales)'),
+(10, 'RGEN REGION 4A (Cavite / Laguna / Batangas / Rizal /\nQuezon)'),
+(11, 'RGEN REGION 4B (Mindoro / Marinduque / Romblon /\nPalawan)'),
+(12, 'RGEN REGION 5 (Masbate / Catanduanes / Sorsogon \nAlbay / Camarines Sur / Camarines Norte)');
 
 -- --------------------------------------------------------
 
@@ -105,14 +136,15 @@ INSERT INTO `information` (`information_id`, `unique_id`, `vest_no`, `aspirant`,
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-`user_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `unique_id` varchar(255) DEFAULT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `forgot_password` varchar(255) DEFAULT NULL,
   `usertype` enum('1','2','3') DEFAULT '3',
   `status` enum('Active','Pending','Ban') DEFAULT NULL,
-  `is_login` datetime NOT NULL
+  `is_login` datetime NOT NULL,
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -126,7 +158,9 @@ CREATE TABLE IF NOT EXISTS `userdata_session` (
   `ip_address` varchar(255) NOT NULL DEFAULT '0',
   `user_agent` varchar(255) NOT NULL,
   `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
-  `user_data` text
+  `user_data` text,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity_idx` (`last_activity`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -134,77 +168,4 @@ CREATE TABLE IF NOT EXISTS `userdata_session` (
 --
 
 INSERT INTO `userdata_session` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('389b57039bb4995bc73be987298acd2c', '::1', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36', 1496419332, '');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `email_sent`
---
-ALTER TABLE `email_sent`
- ADD PRIMARY KEY (`email_sent_id`);
-
---
--- Indexes for table `emergency_info`
---
-ALTER TABLE `emergency_info`
- ADD PRIMARY KEY (`emergency_id`);
-
---
--- Indexes for table `images`
---
-ALTER TABLE `images`
- ADD PRIMARY KEY (`image_id`);
-
---
--- Indexes for table `information`
---
-ALTER TABLE `information`
- ADD PRIMARY KEY (`information_id`), ADD UNIQUE KEY `vest_no` (`vest_no`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
- ADD PRIMARY KEY (`user_id`);
-
---
--- Indexes for table `userdata_session`
---
-ALTER TABLE `userdata_session`
- ADD PRIMARY KEY (`session_id`), ADD KEY `last_activity_idx` (`last_activity`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `email_sent`
---
-ALTER TABLE `email_sent`
-MODIFY `email_sent_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `emergency_info`
---
-ALTER TABLE `emergency_info`
-MODIFY `emergency_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `images`
---
-ALTER TABLE `images`
-MODIFY `image_id` int(10) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `information`
---
-ALTER TABLE `information`
-MODIFY `information_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-MODIFY `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+('22beab4bb693d9b49d283a34ab440a73', '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36', 1498632587, '');
